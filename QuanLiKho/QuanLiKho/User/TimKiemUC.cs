4f530e661985b10a36f0ab868b9e8842aa9225e4
Phiêu Lê Văn <levanphieu99@gmail.com>
2020-09-19 17:48:33 +0700

    tìm kiếm phiếu nhập

--- QuanLiKho/QuanLiKho/User/TimKiemUC.cs
+++ QuanLiKho/QuanLiKho/User/TimKiemUC.cs
@@ -32,16 +32,18 @@ namespace QuanLiKho.User
 
         private void button2_Click(object sender, EventArgs e)
         {
-               dgView.DataSource = da.GetAllPhieuNhap();
-               foreach (string item in da.GetAllMaHH())
-               {
-                    comboBox1.Items.Add(item);
-               }
+               if (kt)
+                    dgView.DataSource = da.TimKiemHHPN(comboBox1.Text);
+               else
+                    dgView.DataSource = da.TimKiemHHPX(comboBox1.Text);
           }
 
         private void button3_Click(object sender, EventArgs e)
         {
-           
+               if (kt)
+                    dgView.DataSource = da.TimKiemDatePN(NgayNhap.Value.ToString("yyyy-MM-dd"), dateTimePicker1.Value.ToString("yyyy-MM-dd"));
+               else
+                    dgView.DataSource = da.TimKiemDatePX(NgayNhap.Value.ToString("yyyy-MM-dd"), dateTimePicker1.Value.ToString("yyyy-MM-dd")
         }
 
         private void button1_Click(object sender, EventArgs e)
