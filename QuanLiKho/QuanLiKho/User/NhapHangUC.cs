00d81443280a0d086a20e5a076e8615a8c562c49
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-10-03 16:08:43 +0700

    Van Anh

--- QuanLiKho/QuanLiKho/User/NhapHangUC.cs
+++ QuanLiKho/QuanLiKho/User/NhapHangUC.cs
@@ -24,7 +24,21 @@ namespace QuanLiKho.User
 
         
 
-       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(da.XoaPhieuNhap(MaHH.Text, MaLoHang.Text, NgayNhap.Value.ToString("yyyy-MM-dd"), NSX.Value.ToString("yyyy-MM-dd"), HSD.Value.ToString("yyyy-MM-dd"), SoLuong.Text))
+            {
+                MessageBox.Show("Xoa Thanh công");
+                MaHH.Text = "";
+                MaLoHang.Text = "";
+                SoLuong.Text = "";
+                dgView.DataSource = displayALLPhieuNhap();
+            }
+            else
+            {
+                MessageBox.Show("Xóa không thành công");
+            }
+        }
 
         private void NhapHangUC_Load(object sender, EventArgs e)
         {
@@ -59,7 +73,7 @@ namespace QuanLiKho.User
 
         
 
-      
+        
     }
 }
 
